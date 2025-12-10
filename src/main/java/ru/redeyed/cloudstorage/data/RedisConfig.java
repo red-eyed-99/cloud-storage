161732f74f1e9caa16af95f28e47d8c9a3a2869e
red-eyed-99 <mgnblack99@gmail.com>
2025-12-10 14:35:01 +0300

    feat: add redis session configuration

---
+++ src/main/java/ru/redeyed/cloudstorage/data/RedisConfig.java
@@ -0,0 +1,21 @@
+package ru.redeyed.cloudstorage.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.RedisSessionRepository;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import java.time.Duration;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisConfig {
+
+    @Value("${spring.data.redis.session-max-inactive-interval}")
+    private Duration sessionMaxInactiveInterval;
+
+    @Autowired
+    public void setSessionMaxInactiveInterval(RedisSessionRepository redisSessionRepository) {
+        redisSessionRepository.setDefaultMaxInactiveInterval(sessionMaxInactiveInterval);
+    }
+}
