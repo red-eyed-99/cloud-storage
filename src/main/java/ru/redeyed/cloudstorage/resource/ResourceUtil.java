ad9a845b1473a7b7954fb69c0ef2e22b15a531a7
red-eyed-99 <mgnblack99@gmail.com>
2025-12-17 12:28:16 +0300

    feat: add utility for working with resource paths

---
+++ src/main/java/ru/redeyed/cloudstorage/resource/ResourceUtil.java
@@ -0,0 +1,87 @@
+package ru.redeyed.cloudstorage.resource;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ResourceUtil {
+
+    public static final String PATH_DELIMITER = "/";
+
+    private static final int CHARACTER_NOT_PRESENT = -1;
+
+    public static String extractResourceName(String path) {
+        return isDirectory(path)
+                ? extractDirectoryName(path)
+                : extractFileName(path);
+    }
+
+    public static String removeResourceName(String path) {
+        return isDirectory(path)
+                ? removeDirectoryName(path)
+                : removeFileName(path);
+    }
+
+    public static boolean isRootDirectory(String path) {
+        return path.equals(PATH_DELIMITER);
+    }
+
+    private static boolean isDirectory(String path) {
+        return path.endsWith(PATH_DELIMITER);
+    }
+
+    private static String extractDirectoryName(String path) {
+        var beforeLastDelimiterIndex = path.length() - 2;
+        var penultimateDelimiterIndex = path.lastIndexOf(PATH_DELIMITER, beforeLastDelimiterIndex);
+
+        var beginIndex = 0;
+        var endIndex = beforeLastDelimiterIndex + 1;
+
+        if (penultimateDelimiterIndex == CHARACTER_NOT_PRESENT) {
+            return path.substring(beginIndex, endIndex);
+        }
+
+        beginIndex = penultimateDelimiterIndex + 1;
+        endIndex = beforeLastDelimiterIndex + 1;
+
+        return path.substring(beginIndex, endIndex);
+    }
+
+    private static String removeDirectoryName(String path) {
+        var beforeLastDelimiterIndex = path.length() - 2;
+        var penultimateDelimiterIndex = path.lastIndexOf(PATH_DELIMITER, beforeLastDelimiterIndex);
+
+        if (penultimateDelimiterIndex == CHARACTER_NOT_PRESENT) {
+            return PATH_DELIMITER;
+        }
+
+        var beginIndex = 0;
+        var endIndex = penultimateDelimiterIndex + 1;
+
+        return path.substring(beginIndex, endIndex);
+    }
+
+    private static String extractFileName(String path) {
+        var lastDelimiterIndex = path.lastIndexOf(PATH_DELIMITER);
+
+        if (lastDelimiterIndex == CHARACTER_NOT_PRESENT) {
+            return path;
+        }
+
+        var beginIndex = lastDelimiterIndex + 1;
+
+        return path.substring(beginIndex);
+    }
+
+    private static String removeFileName(String path) {
+        var lastDelimiterIndex = path.lastIndexOf(PATH_DELIMITER);
+
+        if (lastDelimiterIndex == CHARACTER_NOT_PRESENT) {
+            return PATH_DELIMITER;
+        }
+
+        var beginIndex = 0;
+        var endIndex = lastDelimiterIndex + 1;
+
+        return path.substring(beginIndex, endIndex);
+    }
+}
