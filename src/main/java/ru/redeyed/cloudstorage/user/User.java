9e3ed240a56d2e01caf08043ce72d97434695595
red-eyed-99 <mgnblack99@gmail.com>
2025-12-19 18:51:40 +0300

    feat: remove unnecessary code and migrations

--- src/main/java/ru/redeyed/cloudstorage/user/User.java
+++ src/main/java/ru/redeyed/cloudstorage/user/User.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -14,9 +12,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import ru.redeyed.cloudstorage.common.validation.annotation.BcryptEncoded;
 import ru.redeyed.cloudstorage.common.validation.annotation.ValidUsername;
-import ru.redeyed.cloudstorage.resource.Resource;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -36,9 +31,4 @@ public class User {
 
     @BcryptEncoded(parameterName = "password")
     private String password;
-
-    @OneToMany
-    @JoinColumn(name = "user_id")
-    @Builder.Default
-    private List<Resource> resources = new ArrayList<>();
 }
--- src/main/resources/db/changelog/types/1-create-enum-resource-type.sql
+++
@@ -1,5 +0,0 @@
--- liquibase formatted sql
-
--- changeset red-eyed:create-enum-resource-type
-
-CREATE TYPE RESOURCE_TYPE AS ENUM ('FILE', 'DIRECTORY');
