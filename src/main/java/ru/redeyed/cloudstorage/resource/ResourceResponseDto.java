0fc5954c0af8dbbda2f49768e825bb579c96be5a
red-eyed-99 <mgnblack99@gmail.com>
2025-12-19 19:01:55 +0300

    feat: remove the size parameter from the response body when the resource type is directory

--- src/main/java/ru/redeyed/cloudstorage/resource/ResourceResponseDto.java
+++ src/main/java/ru/redeyed/cloudstorage/resource/ResourceResponseDto.java
@@ -1,4 +1,15 @@
 package ru.redeyed.cloudstorage.resource;
 
-public record ResourceResponseDto(String path, String name, Long size, ResourceType type) {
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+public record ResourceResponseDto(
+
+        String path,
+
+        String name,
+
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        Long size,
+
+        ResourceType type) {
 }
