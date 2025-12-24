e4334e4a0551054bea246c19e9b510adc7edaf77
red-eyed-99 <mgnblack99@gmail.com>
2025-12-24 13:27:17 +0300

    test: add minio testcontainers configuration and change the way test containers are launched

--- src/test/java/ru/redeyed/cloudstorage/test/integration/BaseIntegrationTest.java
+++ src/test/java/ru/redeyed/cloudstorage/test/integration/BaseIntegrationTest.java
@@ -1,18 +1,49 @@
 package ru.redeyed.cloudstorage.test.integration;
 
 import com.redis.testcontainers.RedisContainer;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MinIOContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 
 @IntegrationTest
 public abstract class BaseIntegrationTest {
 
-    @Container
-    @ServiceConnection
     private static final PostgreSQLContainer<?> POSTGRES_TEST_CONTAINER = new PostgreSQLContainer<>("postgres:18.1");
 
-    @Container
-    @ServiceConnection
     private static final RedisContainer REDIS_TEST_CONTAINER = new RedisContainer("redis:8.4.0");
+
+    private static final MinIOContainer MINIO_TEST_CONTAINER = new MinIOContainer("minio/minio:RELEASE.2025-09-07T16-13-09Z-cpuv1");
+
+    @BeforeAll
+    static void startContainers() {
+        POSTGRES_TEST_CONTAINER.start();
+        REDIS_TEST_CONTAINER.start();
+        MINIO_TEST_CONTAINER.start();
+    }
+
+    @DynamicPropertySource
+    static void setContainersProperties(DynamicPropertyRegistry registry) {
+        setPostgresProperties(registry);
+        setRedisProperties(registry);
+        setMinioProperties(registry);
+    }
+
+    private static void setPostgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES_TEST_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES_TEST_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRES_TEST_CONTAINER::getPassword);
+    }
+
+    private static void setRedisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", REDIS_TEST_CONTAINER::getHost);
+        registry.add("spring.data.redis.port", REDIS_TEST_CONTAINER::getFirstMappedPort);
+    }
+
+    private static void setMinioProperties(DynamicPropertyRegistry registry) {
+        registry.add("minio.url", MINIO_TEST_CONTAINER::getS3URL);
+        registry.add("minio.accessKey", MINIO_TEST_CONTAINER::getUserName);
+        registry.add("minio.secretKey", MINIO_TEST_CONTAINER::getPassword);
+    }
 }
--- src/test/java/ru/redeyed/cloudstorage/test/integration/IntegrationTest.java
+++ src/test/java/ru/redeyed/cloudstorage/test/integration/IntegrationTest.java
@@ -4,7 +4,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -15,7 +14,6 @@ import java.lang.annotation.Target;
 @ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
-@Testcontainers
 @Transactional
 public @interface IntegrationTest {
 }
