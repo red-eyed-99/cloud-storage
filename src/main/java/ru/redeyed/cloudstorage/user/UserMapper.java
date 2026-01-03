0aaa5028aff3881fb17c5f4754b6e19c4fb7c84a
red-eyed-99 <mgnblack99@gmail.com>
2026-01-03 10:05:55 +0300

    feat: change unmapped target policy

--- src/main/java/ru/redeyed/cloudstorage/user/UserMapper.java
+++ src/main/java/ru/redeyed/cloudstorage/user/UserMapper.java
@@ -1,10 +1,11 @@
 package ru.redeyed.cloudstorage.user;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 import ru.redeyed.cloudstorage.auth.UserDetailsImpl;
 import ru.redeyed.cloudstorage.user.dto.CreateUserDto;
 
-@Mapper
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 
     User toUser(CreateUserDto createUserDto);
